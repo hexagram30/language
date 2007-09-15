74bf7c5367d6ccac69f5c264283d7f9a8e1e2fa7
Duncan McGreggor <oubiwann@twistedmatrix.com>
2007-09-15 05:31:14 +0000

    2007.09.15
    * Converted to using setuptools.
    * Added a new directory for a combined adytum.ling package.
    
    
    
    git-svn-id: https://pywordgen.svn.sourceforge.net/svnroot/pywordgen@30 6300c3e7-63e6-0310-bc4a-e3b7454cf1d2

--- branches/2007.09-reorg/setup.py
+++ branches/2007.09-reorg/setup.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
 
 setup(name="PyWordGen",
-    version="1.0",
+    version="1.1",
     description="Python Word Generator",
     author="Duncan McGreggor",
-    author_email="duncan@adytumsolutions.com",
-    url="http://pywordgen.sf.net",
+    author_email="duncan@adytums.us",
+    url="http://pywordgen.sourceforge.net",
     packages=[
         'adytum',
-        'adytum.linguistics',
-        'adytum.linguistics.corpora',
-        'adytum.linguistics.syntagma',
+        'adytum.ling',
+        'adytum.ling.corpora',
+        'adytum.ling.syntagma',
     ],
-    data_files=[('adytum/linguistics/corpora',
-        ['lib/linguistics/corpora/afrikaner_wordlist.txt',
-        'lib/linguistics/corpora/arabic_wordlist.txt',
-        'lib/linguistics/corpora/chinese_wordlist.txt',
-        'lib/linguistics/corpora/english_wordlist.txt',
-        'lib/linguistics/corpora/french_wordlist.txt',
-        'lib/linguistics/corpora/german_wordlist.txt',
-        'lib/linguistics/corpora/hindi_wordlist.txt',
-        'lib/linguistics/corpora/japanese_wordlist.txt',
-        'lib/linguistics/corpora/korean_wordlist.txt',
-        'lib/linguistics/corpora/russian_wordlist.txt',
-        'lib/linguistics/corpora/spanish_wordlist.txt']
+    namespace_packages = ['adytum'],
+    data_files=[('adytum/ling/corpora',
+        ['adytum/ling/corpora/afrikaner_wordlist.txt',
+        'adytum/ling/corpora/arabic_wordlist.txt',
+        'adytum/ling/corpora/chinese_wordlist.txt',
+        'adytum/ling/corpora/english_wordlist.txt',
+        'adytum/ling/corpora/french_wordlist.txt',
+        'adytum/ling/corpora/german_wordlist.txt',
+        'adytum/ling/corpora/hindi_wordlist.txt',
+        'adytum/ling/corpora/japanese_wordlist.txt',
+        'adytum/ling/corpora/korean_wordlist.txt',
+        'adytum/ling/corpora/russian_wordlist.txt',
+        'adytum/ling/corpora/spanish_wordlist.txt']
     )],
-    package_dir = {'adytum': 'lib'},
 )
