2478e65af2e7d604f5f90c4a04570d279a2d3e0a
Duncan McGreggor <oubiwann@twistedmatrix.com>
2007-09-15 23:06:14 +0000

    * Added corpora.files for standardizing access to corpora resources.
    git-svn-id: https://pywordgen.svn.sourceforge.net/svnroot/pywordgen@41 6300c3e7-63e6-0310-bc4a-e3b7454cf1d2

---
+++ branches/2007.09-reorg/wordgen/corpora/files.py
@@ -0,0 +1,9 @@
+"""
+This module defines a way of extracting the corpora text files from the python
+library paths.
+"""
+basePath = './corpora'
+vowelFileTmpl = basePath + "/vowels/%s.txt"
+consonantFileTmpl = basePath + "/consonants/%s.txt"
+statsFileTmpl = basePath + "/stats/%s.txt"
+sourceFileTmpl = basePath + "/sources/%s.txt"
