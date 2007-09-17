90cc37fbd2a640ecbeb83d97faf5e468e792d700
Duncan McGreggor <oubiwann@twistedmatrix.com>
2007-09-17 04:20:46 +0000

    * Added test files for Finnish and Quenya.
    git-svn-id: https://pywordgen.svn.sourceforge.net/svnroot/pywordgen@64 6300c3e7-63e6-0310-bc4a-e3b7454cf1d2

--- trunk/wordgen/utils.py
+++ trunk/wordgen/utils.py
@@ -12,16 +12,18 @@ class Counter(dict):
         
 class UTF8File(object):
     
-    def __init__(self, filename=None, mode='r'):
+    def __init__(self, filename='', mode='r'):
         self.filename = filename
         if filename:
             self.open(filename, mode)
 
-    def open(self, filename, mode):
-        if filename:
-            self.fh = codecs.open(filename, mode, 'utf-8')
-            return self
-        raise FileNotFound
+    def open(self, filename='', mode='r'):
+        if not filename:
+            filename = self.filename
+        if not filename:
+            raise FileNotFound
+        self.fh = codecs.open(filename, mode, 'utf-8')
+        return self
     
     def write(self, data):
         self.fh.write(data)
