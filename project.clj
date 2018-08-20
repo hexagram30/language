48b0651bb349ca8cd3f1e010ee7094db6fffcb0f
Duncan McGreggor <oubiwann@gmail.com>
2018-08-20 17:14:41 -0500

    Updated project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,91 @@
-(defproject hxgm30 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+(defn get-banner
+  []
+  (try
+    (str
+      (slurp "resources/text/banner.txt")
+      ;(slurp "resources/text/loading.txt")
+      )
+    ;; If another project can't find the banner, just skip it;
+    ;; this function is really only meant to be used by Dragon itself.
+    (catch Exception _ "")))
+
+(defn get-prompt
+  [ns]
+  (str "\u001B[35m[\u001B[34m"
+       ns
+       "\u001B[35m]\u001B[33m λ\u001B[m=> "))
+
+(defproject hexagram30/language "0.1.0-SNAPSHOT"
+  :description "A language and word generator for use in hexagram30 narratives"
+  :url "https://github.com/hexagram30/language"
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [clojusc/system-manager "0.3.0-SNAPSHOT"]
+    [clojusc/twig "0.3.3"]
+    [hexagram30/common "0.1.0-SNAPSHOT"]
+    [org.clojure/clojure "1.9.0"]]
+  :profiles {
+    :ubercompile {
+      :aot :all}
+    :dev {
+      :dependencies [
+        [clojusc/trifl "0.3.0"]
+        [org.clojure/tools.namespace "0.2.11"]]
+      :plugins [
+        [lein-shell "0.5.0"]
+        [venantius/ultra "0.5.2"]]
+      :source-paths ["dev-resources/src"]
+      :repl-options {
+        :init-ns hxgm30.language.repl
+        :prompt ~get-prompt
+        :init ~(println (get-banner))}}
+    :lint {
+      :source-paths ^:replace ["src"]
+      :test-paths ^:replace []
+      :plugins [
+        [jonase/eastwood "0.2.9"]
+        [lein-ancient "0.6.15"]
+        [lein-bikeshed "0.5.1"]
+        [lein-kibit "0.1.6"]
+        [venantius/yagni "0.1.4"]]}
+    :test {
+      :plugins [
+        [lein-ltest "0.3.0"]]
+      :test-selectors {
+        :unit #(not (or (:integration %) (:system %)))
+        :integration :integration
+        :system :system
+        :default (complement :system)}}}
+  :aliases {
+    ;; Dev Aliases
+    "repl" ["do"
+      ["clean"]
+      ["repl"]]
+    "ubercompile" ["do"
+      ["clean"]
+      ["with-profile" "+ubercompile" "compile"]]
+    "check-vers" ["with-profile" "+lint" "ancient" "check" ":all"]
+    "check-jars" ["with-profile" "+lint" "do"
+      ["deps" ":tree"]
+      ["deps" ":plugin-tree"]]
+    "check-deps" ["do"
+      ["check-jars"]
+      ["check-vers"]]
+    "kibit" ["with-profile" "+lint" "kibit"]
+    "eastwood" ["with-profile" "+lint" "eastwood" "{:namespaces [:source-paths]}"]
+    "lint" ["do"
+      ["kibit"]
+      ["eastwood"]]
+    "ltest" ["with-profile" "+test" "ltest"]
+    "ltest-clean" ["do"
+      ["clean"]
+      ["ltest"]]
+    "build" ["do"
+      ["clean"]
+      ["check-vers"]
+      ["lint"]
+      ["ltest" ":all"]
+      ["uberjar"]]})
+
