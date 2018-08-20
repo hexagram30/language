61170bbc9e05b5f8e9e3a70f589c79856b7670ed
Duncan McGreggor <oubiwann@gmail.com>
2018-08-20 17:18:15 -0500

    Started adding Clojure util fns.

--- src/hxgm30/language/syntagmata/util.clj
+++ src/hxgm30/language/syntagmata/util.clj
@@ -1,40 +1,15 @@
-import codecs
+(ns hxgm30.language.syntagmata.util
+  (:require
+    [clojure.java.io :as io]
+    [clojure.string :as string]))
 
-from anamyeter.exceptions import FileNotFound
-
-
-class Counter(dict):
-
-    def updateCount(self, key):
-        try:
-            self[key] += 1
-        except KeyError:
-            self.setdefault(key, 1)
-
-
-class UTF8File(object):
-    
-    def __init__(self, filename='', mode='r'):
-        self.filename = filename
-        if filename:
-            self.open(filename, mode)
-
-    def open(self, filename='', mode='r'):
-        if not filename:
-            filename = self.filename
-        if not filename:
-            raise FileNotFound
-        self.fh = codecs.open(filename, mode, 'utf-8')
-        return self
-    
-    def write(self, data):
-        self.fh.write(data)
-    
-    def read(self):
-        return self.fh.read()
-
-    def readlines(self):
-        return self.fh.readlines()
-
-    def close(self):
-        self.fh.close()
+(defn load-corpora
+  [type language]
+  (->> (format "syntagmata/corpora/%s/%s.txt"
+               (name type)
+               (name language))
+       io/resource
+       io/reader
+       line-seq
+       (mapcat #(string/split % #" "))
+       sort))
