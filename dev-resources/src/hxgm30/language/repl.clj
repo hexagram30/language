a486a286677d6e48235016b15b2593d5f65aeda1
Duncan McGreggor <oubiwann@gmail.com>
2018-08-20 17:18:44 -0500

    Commented out system code for now.

--- dev-resources/src/hxgm30/language/repl.clj
+++ dev-resources/src/hxgm30/language/repl.clj
@@ -7,41 +7,42 @@
     [clojusc.system-manager.core :refer :all]
     [clojusc.twig :as logger]
     [com.stuartsierra.component :as component]
-    [hxgm30.language.components.config :as config]
-    [hxgm30.language.components.core]
+    ; [hxgm30.language.components.config :as config]
+    ; [hxgm30.language.components.core]
+    [hxgm30.language.syntagmata.util :as util]
     [trifl.java :refer [show-methods]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Initial Setup & Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def setup-options {
-  :init 'hxgm30.language.components.core/init
-  :after-refresh 'hxgm30.language.repl/init-and-startup
-  :throw-errors false})
+; (def setup-options {
+;   :init 'hxgm30.language.components.core/init
+;   :after-refresh 'hxgm30.language.repl/init-and-startup
+;   :throw-errors false})
 
-(defn init
-  "This is used to set the options and any other global data.
+; (defn init
+;   "This is used to set the options and any other global data.
 
-  This is defined in a function for re-use. For instance, when a REPL is
-  reloaded, the options will be lost and need to be re-applied."
-  []
-  (logger/set-level! '[hxgm30] :debug)
-  (setup-manager setup-options))
+;   This is defined in a function for re-use. For instance, when a REPL is
+;   reloaded, the options will be lost and need to be re-applied."
+;   []
+;   (logger/set-level! '[hxgm30] :debug)
+;   (setup-manager setup-options))
 
-(defn init-and-startup
-  "This is used as the 'after-refresh' function by the REPL tools library.
-  Not only do the options (and other global operations) need to be re-applied,
-  the system also needs to be started up, once these options have be set up."
-  []
-  (init)
-  (startup))
+; (defn init-and-startup
+;   "This is used as the 'after-refresh' function by the REPL tools library.
+;   Not only do the options (and other global operations) need to be re-applied,
+;   the system also needs to be started up, once these options have be set up."
+;   []
+;   (init)
+;   (startup))
 
 ;; It is not always desired that a system be started up upon REPL loading.
 ;; Thus, we set the options and perform any global operations with init,
 ;; and let the user determine when then want to bring up (a potentially
 ;; computationally intensive) system.
-(init)
+;;(init)
 
 (defn banner
   []
