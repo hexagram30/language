ab33f74f1a4e165eeb4a10b8a607f9a35bee0e08
Duncan McGreggor <oubiwann@gmail.com>
2018-08-20 17:59:44 -0500

    Added fns for loading vowels, cons, & alphabets.

--- src/hxgm30/language/syntagmata/util.clj
+++ src/hxgm30/language/syntagmata/util.clj
@@ -1,6 +1,7 @@
 (ns hxgm30.language.syntagmata.util
   (:require
     [clojure.java.io :as io]
+    [clojure.set :as set]
     [clojure.string :as string]))
 
 (defn load-corpora
@@ -13,3 +14,23 @@
        line-seq
        (mapcat #(string/split % #" "))
        sort))
+
+(defn load-oneline-file
+  [type language]
+  (->> language
+       (load-corpora type)
+       first
+       (map str)
+       set))
+
+(defn load-consonants
+  [language]
+  (load-oneline-file :consonants language))
+
+(defn load-vowels
+  [language]
+  (load-oneline-file :vowels language))
+
+(defn load-alphabet
+  [language]
+  (set/union (load-consonants language) (load-vowels language)))
