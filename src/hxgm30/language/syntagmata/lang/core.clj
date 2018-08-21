72a850c8ffbede1c2d168966b361073d22b5b25a
Duncan McGreggor <oubiwann@gmail.com>
2018-08-21 11:52:24 -0500

    Added list of supported languages for syntagmata.

---
+++ src/hxgm30/language/syntagmata/lang/core.clj
@@ -0,0 +1,19 @@
+(ns hxgm30.language.syntagmata.lang.core)
+
+(def supported
+  [:afrikaans
+   :arabic
+   :chinese
+   :english
+   :french
+   :german
+   :hindi
+   :japanese
+   :korean
+   :oldenglish
+   :oldnorse
+   :pie
+   :russian
+   :sanskrit
+   :scots
+   :spanish])
