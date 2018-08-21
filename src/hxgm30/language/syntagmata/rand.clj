8ad0b57c81c5425595e28c787a4227057cead7f5
Duncan McGreggor <oubiwann@gmail.com>
2018-08-21 11:59:10 -0500

    Updated word to use new positional syllable support.

--- src/hxgm30/language/syntagmata/rand.clj
+++ src/hxgm30/language/syntagmata/rand.clj
@@ -10,23 +10,39 @@
   (util/percent-> (rand) (get-in stats [:pseudo-syllables :percent-ranges])))
 
 (defn syllable
-  [stats]
-  (util/percent-> (rand) (get-in stats [:sound-transitions :percent-ranges])))
+  [stats position]
+  (case position
+    :initial (util/percent->
+              (rand)
+              (get-in stats [:sound-transitions :initial :percent-ranges]))
+    :medial (util/percent->
+              (rand)
+              (get-in stats [:sound-transitions :medial :percent-ranges]))
+    :final (util/percent->
+              (rand)
+              (get-in stats [:sound-transitions :final :percent-ranges]))))
 
 (defn word
-  [stats]
-  (->> stats
-       syllable-count
-       inc
-       range
-       (mapcat (fn [_] (syllable stats)))
-       (apply str)))
+  ([stats]
+    (word stats (syllable-count stats)))
+  ([stats syllables]
+    (case syllables
+      1 (syllable stats :initial)
+      2 (str (syllable stats :initial)
+             (syllable stats :final))
+      (str (syllable stats :initial)
+           (->> syllables
+                dec
+                range
+                (mapcat (fn [_] (syllable stats :medial)))
+                (string/join ""))
+           (syllable stats :final)))))
 
 (defn sentence
   ([stats]
     (sentence stats (rand-int 10)))
-  ([stats word-count]
-    (->> word-count
+  ([stats words]
+    (->> words
          inc
          range
          (map (fn [_] (word stats)))
