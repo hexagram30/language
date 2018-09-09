784025062da7aea8b4819cd32badd23230074a6c
Duncan McGreggor <oubiwann@gmail.com>
2018-09-09 18:11:52 -0500

    Fixed CLI to use the right objects when regen'ing stats.

--- src/hxgm30/language/cli.clj
+++ src/hxgm30/language/cli.clj
@@ -7,31 +7,40 @@
     [hxgm30.language.components.lang :as lang-component]
     [hxgm30.language.gen.assembled.core :as asm]
     [hxgm30.language.gen.core :as gen]
-    [hxgm30.language.gen.name :as name]))
+    [hxgm30.language.gen.name :as name]
+    [taoensso.timbre :as log]))
 
 (defn setup-system
   []
-  (logger/set-level! '[hxgm30] :error)
-  (system-manager/setup-manager {:init 'hxgm30.language.components.core/cli
-                                 :throw-errors true})
+  (logger/set-level! '[hxgm30] :trace)
+  (system-manager/setup-manager {:init 'hxgm30.language.components.core/init
+                                 :throw-errors false})
   (system-manager/startup)
   (system-manager/system))
 
 (defn -main
   [& args]
   (let [cmd (keyword (first args))
+        _ (log/debug "Got command:" cmd)
         sys (setup-system)
+        _ (log/debug "System, setup complete.")
         gen-type (config/lang-default-generator-mode sys)
+        _ (when-not (nil? gen-type) (log/debug "Got generator type:" gen-type))
         generator (lang-component/get-generator sys gen-type)]
+    (log/debug "Got generator:" generator)
     (case cmd
       :regen-markov-chains (do
                              (println "Regenerating markov-chain data ...\n")
-                             (gen/regen-stats
-                              (lang-component/get-generator sys :markov)))
+                             (->> :markov
+                                  (lang-component/get-generator sys)
+                                  :stats-gen
+                                  gen/regen-stats))
       :regen-syntagmata (do
                           (println "Regenerating syntagmata data ...\n")
-                          (gen/regen-stats
-                           (lang-component/get-generator sys :syntagmata)))
+                          (->> :syntagmata
+                               (lang-component/get-generator sys)
+                               :stats-gen
+                               gen/regen-stats))
       :names (if-let [race (second args)]
                (name/run generator (keyword race))
                (name/run generator))
@@ -39,4 +48,3 @@
                       language (keyword (second args))]
                   (asm/run generator world language)))
     (println)))
-
