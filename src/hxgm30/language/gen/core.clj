b856e31bdcaac80a7562d738bb6ae83270685491
Duncan McGreggor <oubiwann@gmail.com>
2019-09-07 17:19:32 -0500

    Added support for generating words based on other words.
    Fixes #42.

--- src/hxgm30/language/gen/core.clj
+++ src/hxgm30/language/gen/core.clj
@@ -1,5 +1,6 @@
 (ns hxgm30.language.gen.core
   (:require
+    [hxgm30.common.util :as util]
     [hxgm30.language.gen.impl.markov :as markov]
     [hxgm30.language.gen.impl.syntagmata :as syntagmata])
   (:import
@@ -49,3 +50,83 @@
   (case type
     :markov (markov/create-content-generator system)
     :syntagmata (syntagmata/create-content-generator system)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Non-system-using functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn eat [data]
+  (loop [d (map identity data)
+         acc [d]]
+    (if (= (count d) 2)
+      acc
+      (let [nxt (vec (rest d))]
+        (recur nxt
+               (conj acc nxt))))))
+
+(defn simple-markov [word]
+  (reduce (fn [acc rem] (assoc acc (str (first rem)) (str (second rem))))
+          {}
+          (eat word)))
+
+(defn create-word-lookup [words-data]
+  "This function expects `words-data` to have a structure like the following:
+
+  ```
+  (def world
+    [{:word \"tunkasila\"
+      :weight 52}
+     {:word \"nuna\"
+      :weight 4}
+     {:word \"olor\"
+      :weight 3}
+     {:word \"shaoghal\"
+      :weight 3}
+     {:word \"arda\"
+      :weight 1}
+     {:word \"verǫld\"
+      :weight 13}
+     {:word \"maailma\"
+      :weight 2}
+     {:word \"delkhiin\"
+      :weight 5}
+     {:word \"sikul\"
+      :weight 5}
+     {:word \"honua\"
+      :weight 3}
+     {:word \"ao\"
+      :weight 9}])
+  ````
+  "
+  (reduce (partial util/frequency->percent-range total)
+          []
+          (map #(list (simple-markov (:word %)) (:weight %))
+               words-data)))
+
+(defn derive-word
+  "Given a data structure that's a list of words and their weights, synthesize
+  a new word based on the given words with word of higher weight contributed
+  their sounds more heavily to the generated word.
+
+  For the data structure, see the docstring of the `create-word-lookup`
+  function."
+  ([words-data]
+    (derive-word words-data (rand-int 10)))
+  ([words-data syllable-count]
+    (let [total (reduce + 0.0 (map :weight words-data))]
+      (apply str
+        (flatten
+          (map (fn [_] (rand-nth
+                        (vec
+                         (u/percent-> (rand)
+                                      (create-word-lookup words-data)))))
+               (range syllable-count)))))))
+
+(defn derive-words
+  ([words-data]
+    (derive-words words-data 10))
+  ([words-data word-count]
+    (map (fn [_] (derive-word words-data)) (range word-count)))
+  ([words-data word-count syllable-count]
+    (map (fn [_] (derive-word words-data syllable-count))
+         (range word-count))))
